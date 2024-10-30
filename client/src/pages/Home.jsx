be5c308482c5ec2f2a4e07c49c0a941e9e44ce89
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-10-29 20:29:32 -0400

    modified the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,44 +1,41 @@
 import { useGlobalContext } from "../utils/GlobalState";
-import { QUERY_USERS } from "../utils/queries";
+import { QUERY_TRIPS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 import { useEffect } from "react";
-import { SET_USERS } from "../utils/actions";
+import { SET_TRIPS } from "../utils/actions";
 
 const Home = () => {
 
     
     const [state, dispatch] = useGlobalContext();
-    const {data, loading} = useQuery(QUERY_USERS);
+    const {data, loading} = useQuery(QUERY_TRIPS);
     
     
-    const users = data?.users || []
-    console.log(users);
+    const trips = data?.trips || []
+    console.log(trips);
 
     useEffect(() => {
-        dispatch({type: SET_USERS, payload: users})
-    }, [data])
+        dispatch({type: SET_TRIPS, payload: trips})
+    }, [data, dispatch])
 
     if (loading) {
         return <h3>Loading...</h3>
     }
-    const publicTrips = users.flatMap(user => 
-      user.trips
+    const publicTrips = trips
           .filter(trip => trip.public)
           .map(trip => ({
               ...trip,
-              author: user.username, 
-              preview: trip.description.split(" ").slice(0, 50).join(" ") 
-          }))
-  );
+              preview: trip.details.split(" ").slice(0, 50).join(" ") 
+          }));
   return (
     <div className="container">
-     <h1>Travel Blog</h1>
+     <h1>WRLD TRAVELLERS</h1>
      {publicTrips.map(trip => (
                 <div key={trip.id} className="trip-card">
-                    <h2>{trip.title}</h2>
+                    <h2>{trip.name}</h2>
                     <p><strong>Destination:</strong> {trip.destination}</p>
                     <p><strong>Date:</strong> {trip.date}</p>
-                    <p><strong>Author:</strong> {trip.author}</p>
+                    <p><strong>Author:</strong> {trip.userId}</p>
                     <p>{trip.preview}...</p>
                 </div>
             ))}
