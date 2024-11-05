7841d15a9f6853c6fc9f31a08fc497caa5727bcc
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-11-04 22:10:25 -0500

    Cong1104_ready for merge

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,18 +1,18 @@
 const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const { User, Trip } = require('../models');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   await cleanDB('User', 'users');
 
-   await User.create({
+   const user1 = await User.create({
     firstName: 'Pamela',
     lastName: 'Washington',
     email: 'pamela@testmail.com',
     password: 'password12345',
   });
 
-  await User.create({
+  const user2 = await User.create({
     firstName: 'Elijah',
     lastName: 'Holt',
     email: 'eholt@testmail.com',
@@ -20,6 +20,32 @@ db.once('open', async () => {
   });
 
   console.log('users seeded');
+  await Trip.create([
+    {
+      name: 'A 3-day trip to NYC',
+      destination: 'New York City',
+      text: 'I had a 3-day trip to NYC with my friends. We visited museums, historical landmarks, and tried diverse cuisines. However, there are still many things that we did not have opportunities to explore. Looking forward to coming back soon!!!',
+      isPublic: true,
+      category: 'Urban Exploration', 
+      user: user1._id, 
+      createdAt: new Date(), 
+      pictures: [], 
+    },
+    {
+      name: 'Sunny Beach Vacation',
+      destination: 'Miami',
+      text: 'A relaxing trip to the sunny beaches of Miami. Visited the Florida Keys as well.',
+      isPublic: true,
+      category: 'Beach', 
+      user: user2._id, 
+      createdAt: new Date(), 
+      pictures: [], 
+    }
+  ]);
+
+  console.log('Trips seeded');
 
   process.exit();
 });
+
+
